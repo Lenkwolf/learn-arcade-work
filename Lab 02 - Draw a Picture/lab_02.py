586d801dc9bd0dae14302fb7e1b092cec8990ac0
Lenkwolf <puggychien@gmail.com>
2021-04-09 09:46:41 +1200

    A

--- Lab 02 - Draw a Picture/lab_02.py
+++ Lab 02 - Draw a Picture/lab_02.py
@@ -9,13 +9,21 @@ def on_draw(dt):
 
 
     arcade.draw_circle_filled(on_draw.x,on_draw.y,50,arcade.color.RED)
-    on_draw.x += 1
-    on_draw.y += 3
-
-
+    on_draw.x += on_draw.vx
+    on_draw.y += on_draw.vy
+    if on_draw.x < 0:
+        on_draw.vx =abs(on_draw.vx)
+    if on_draw.x < WIDTH:
+        on_draw.vx =abs(on_draw.vx)
+    if on_draw.y < 0:
+        on_draw.vy =abs(on_draw.vy)
+    if on_draw.x < 0:
+        on_draw.vx =abs(on_draw.vx)
 on_draw.x = 100
 on_draw.y = 100
 
+on_draw.vx = 2
+on_draw.vy = 2
 arcade.open_window(WIDTH,HEIGHT,'bounce')
 arcade.set_background_color(arcade.color.GRAY)
 arcade.schedule(on_draw, 1/60)
