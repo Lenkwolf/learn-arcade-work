bf514d04b09927d60a2627679fc48b75eef1ee7d
Lenkwolf <puggychien@gmail.com>
2021-05-03 10:52:49 +1200

    m

--- Lab 08 - Sprites/lab_08.py
+++ Lab 08 - Sprites/lab_08.py
@@ -9,7 +9,31 @@ COIN_COUNT = 50
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
-class Game(arcade.window):
+class Game(arcade.Window):
 
     def __init__(self):
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, 'shprite example')
+
+        self.player_list = arcade.sprite_list()
+        self.coin_list = arcade.sprite_list()
+
+        self.score = 0
+
+        self.player_sprite = arcade.Sprite("gorilla.png", SPRITE_SCALING_PLAYER)
+        self.player_sprite.center_x = 50
+        self.player_sprite.center_y = 50
+        self.player_list.append(self.player_sprite)
+        
+        self.set_mouse_visible(False)
+
+        arcade.set_background_color(arcade.color.AMAZON)
+
+    def on_draw(self):
+        arcade.start_render()
+
+def main():
+    Window = Game()
+    arcade.run
+
+if __name__ == '__main__':
+    main()
